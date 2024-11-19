a5da9c3edeccc6bb311182dfe4bfd2ec3209df5a
NicoleCarvalh <nicolecarvalho6@gmail.com>
2024-11-19 15:21:29 -0300

    Removing vercel.json file

---
+++ test.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+print(f"Supabase URL: {SUPABASE_URL}")
+print(f"Supabase Key: {SUPABASE_KEY}")
